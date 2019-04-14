412c5818544c59c076ce6c821776870f97537fe2
Michael King <mking@lyonscg.com>
2019-04-14 17:17:13 -0400

    functioning product search setup

--- app.js
+++ app.js
@@ -40,47 +40,10 @@ app.post('/webhook/', (req, res) => {
 		
 	switch (actionName) {
 		case 'test':
-			{
-				text = "Response from Heroku";
-				messageData = {
-					fulfillmentText: text
-				};
-				res.send(messageData);
-			}
+			testResponse(res);
 			break;
 		case 'looking-for':
-			{
-				console.log("Looking For Search");
-				var query = data.queryResult.parameters.query;
-				productSearch.search(query,(err,data)=> {
-					if (err){
-						messageData = {
-							fulfillmentText: err
-						};
-					}else if (data.count == 0) {
-						messageData = {
-							fulfillmentText: "No results found for: " + query
-						};
-					}else {
-						// results retrieved 
-						var fulfillmentMessages = [];
-						for(let i = 0; i < data.count; i++){
-							var result = data.hits[i];
-							fulfillmentMessages.push({
-								card: {
-									title: result.product_name,
-									subtitle: result.price,
-									imageUri: result.image.link
-								}
-							});
-						}
-						messageData = {
-							fulfillmentMessages: fulfillmentMessages
-						};
-					}
-					res.send(messageData);
-				});
-			}
+			lookingFor(data, res);
 			break;
 		default:
 			//unhandled action
@@ -92,4 +55,48 @@ app.post('/webhook/', (req, res) => {
 // Spin up the server
 app.listen(app.get('port'), function () {
 	console.log('running on port', app.get('port'))
-})
+});
+
+function lookingFor(data, res) {
+	console.log("Search triggered for: " + query);
+	var query = data.queryResult.parameters.query;
+	productSearch.search(query, (err, data) => {
+		if (err) {
+			messageData = {
+				fulfillmentText: err
+			};
+		}
+		else if (data.count == 0) {
+			messageData = {
+				fulfillmentText: "No results found for: " + query
+			};
+		}
+		else {
+			// results retrieved 
+			var fulfillmentMessages = [];
+			for (let i = 0; i < data.count; i++) {
+				var result = data.hits[i];
+				fulfillmentMessages.push({
+					card: {
+						title: result.product_name,
+						subtitle: result.price,
+						imageUri: result.image.link
+					}
+				});
+			}
+			messageData = {
+				fulfillmentMessages: fulfillmentMessages
+			};
+		}
+		res.send(messageData);
+	});
+}
+
+function testResponse(res) {
+	text = "Response from Heroku";
+	messageData = {
+		fulfillmentText: text
+	};
+	res.send(messageData);
+}
+
